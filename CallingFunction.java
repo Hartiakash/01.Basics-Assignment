b6702a7caf072125cdfe57a6af3033db7b18fa52
Akash Harti <157797946+Hartiakash@users.noreply.github.com>
2026-06-09 18:09:34 +0530

    Clean up comments in CallingFunction.java
    Removed author information and website reference from comments.

--- CallingFunction.java
+++ CallingFunction.java
@@ -1,8 +1,6 @@
 package Basics_Assignment;
 
 /**
- * @author JALA Academy
- * Website Reference : https://java.jalaacademy.com/java/java_basics.html
  *
  * 6. Write a function to print your name and call the function from main method.
  */
